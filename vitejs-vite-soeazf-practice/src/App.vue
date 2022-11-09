95c849fde6dd6418fb83ae9a0b5e6723e2406422
Ebenezer Jesuraj <ebenezer@beezlabs.com>
2022-11-09 10:26:44 +0530

    ran the setup locally and have added relevant git config files and other run files in for the record..
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>

---
+++ vitejs-vite-soeazf-practice/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'</script>
+
+<template>
+  <div>
+  </div>
+  <HelloWorld msg="Vite + Vue"/>
+</template>
+
+<template>
+
+  <div>
+
+    <HelloWorld Vue msg="Vite  "
+    " />
+
+    //Sing me to Sleep
+
+    //Nested Template..
+
+    <template>
+
+      <div>
+        <HelloWorld msg="Vite + Vue"/>
+      </div>
+
+    </template>
+
+  </div>
+
+</template>
+<style scoped>
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ vitejs-vite-soeazf-practice/src/main.js
@@ -0,0 +1,13 @@
+import {createApp} from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// Compare this snippet from private\lib\Foo.vue:
+
+// Compare this snippet from private\lib\Bar.vue:
+
+// Compare this snippet from
+
+// private\lib\index.html
